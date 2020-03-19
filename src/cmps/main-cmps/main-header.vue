c51a55fbf6b16723b97f43fd8e7fda1e7b73ea5a
rami davidov <ramusdavidoff@gmail.com>
2020-03-19 18:57:28 +0200

    basic layout

--- src/cmps/main-cmps/main-header.vue
+++ src/cmps/main-cmps/main-header.vue
@@ -1,11 +1,12 @@
 <template>
   <section class="main-header">
-      <h1>Main-Header</h1>
+    <h1>Main-Header</h1>
+    <router-link to="/">Home</router-link>|
+    <router-link to="/about">About</router-link>|
+    <router-link to="/dashboard">Dashboard</router-link>
   </section>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
