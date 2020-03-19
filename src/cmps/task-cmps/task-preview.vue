21b8df7a010d278209d5615834d88395c4b9fb2e
rami davidov <ramusdavidoff@gmail.com>
2020-03-19 19:59:25 +0200

    starting preview cmp

--- src/cmps/task-cmps/task-preview.vue
+++ src/cmps/task-cmps/task-preview.vue
@@ -1,12 +1,15 @@
 <template>
   <section class="task-preview">
-      <h1>Task-Preview</h1>
+      <h1>{{task.name}}</h1>
       <pre>{{task}}</pre>
   </section>
 </template>
 
 <script>
 export default {
+  props: {
+    task: Object
+  }
 
 }
 </script>
