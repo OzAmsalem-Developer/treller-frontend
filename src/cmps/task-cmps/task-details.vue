a02bdb1f0dcfdab0579ca3e4a1a04eba3a5a2194
rami davidov <ramusdavidoff@gmail.com>
2020-03-19 23:03:13 +0200

    task-details cmp

--- src/cmps/task-cmps/task-details.vue
+++ src/cmps/task-cmps/task-details.vue
@@ -2,35 +2,55 @@
   <section v-if="task" class="task-details">
     <h1>{{task.name}}</h1>
 
-    <div class="task-labels">
+    <section v-if="task.labels" class="task-labels">
       <span>Labels:</span>
       <span>Update</span>
       <div class="members-list">
         <div v-for="label in labels" :key="label.id">{{label.txt}}</div>
         <button>+</button>
       </div>
-    </div>
+    </section>
 
-    <div class="task-members">
+    <section v-if="task.members" class="task-members">
       <span>Members:</span>
       <span>Invite</span>
       <div class="members-list">
-        <div v-for="member in members" :key="member.id">{{member.fullName}}</div>
+        <div v-for="member in task.members" :key="member.id">{{member.fullName}}</div>
         <button>+</button>
       </div>
-    </div>
+    </section>
 
-    <div class="task-due-date">
+    <section class="task-due-date">
       <button>[]</button>
       <span>Calender:</span>
-      <span>{{task.dueDate}}:</span>
-    </div>
+      <span v-if="task.dueDate">{{task.dueDate}}:</span>
+    </section>
 
-    <div class="task-description">
+    <section v-if="task.description" class="task-description">
       <span>Description:</span>
       <span>Edit</span>
       <p>{{task.description}}:</p>
-    </div>
+    </section>
+
+    <section v-if="task.checklist" class="task-checklist-">
+      <span>{{task.checklist}}</span>
+      <ul class="task-checklist-items">
+        <li v-for="item in task.checklist" :key="item.id"></li>
+      </ul>
+      <input class="checklist-add-item" type="text" placeholder="add an item" />
+    </section>
+
+    <section class="task-discussion">
+      <span>Discussion:</span>
+      <input class="checklist-add-item" type="text" placeholder="Write a comment" />
+      <ul v-if="task.comments.length" class="discussion-cmts">
+        <li v-for="cmt in comments" :key="cmt.id">
+          <span>{{cmt.from}}</span>
+          <span>{{cmt.txt}}</span>
+          <span>{{cmt.createdAt}}</span>
+        </li>
+      </ul>
+    </section>
   </section>
 </template>
 
@@ -54,7 +74,7 @@ export default {
     }
   },
   created() {
-    this.loadTask();
+    // this.loadTask();
   }
 };
 </script>
