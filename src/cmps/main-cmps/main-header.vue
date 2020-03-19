dd4c5c4ec9fc93fbc0319454a97b21f4b7b9dfe6
rami davidov <ramusdavidoff@gmail.com>
2020-03-19 19:46:37 +0200

    main header, basic layout

--- src/cmps/main-cmps/main-header.vue
+++ src/cmps/main-cmps/main-header.vue
@@ -1,9 +1,11 @@
 <template>
   <section class="main-header">
-    <h1>Main-Header</h1>
-    <router-link to="/">Home</router-link>|
-    <router-link to="/about">About</router-link>|
-    <router-link to="/dashboard">Dashboard</router-link>
+    <h1 class="main-logo">Treller</h1>
+    <nav class="nav-links">
+      <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link>|
+      <router-link to="/dashboard">Dashboard</router-link>
+    </nav>
   </section>
 </template>
 
