7f7b835c299246bdf298a2f09b86638a2327c4a5
Guy Amsalem <you@example.com>
2020-03-20 17:34:23 +0200

    label preview scss connection

--- src/cmps/task-cmps/previews/label-preview.vue
+++ src/cmps/task-cmps/previews/label-preview.vue
@@ -1,15 +1,18 @@
 <template>
-  <section class="label-preview"> 
-      <h2>hi</h2>
+  <section class="label-preview">
+    <div class="label-card" v-for="label in labels" :key="label">
+        {{label}}
+    </div>
   </section>
 </template>
 
 <script>
 export default {
-
-}
+  props: {
+    labels: Array
+  }
+};
 </script>
 
 <style>
-
 </style>
