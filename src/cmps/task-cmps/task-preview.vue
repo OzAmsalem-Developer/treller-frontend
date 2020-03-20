2104c1a4251c8d72be699d66bf018de9502339e5
Guy Amsalem <you@example.com>
2020-03-20 16:59:43 +0200

    Add label preview, start working

--- src/cmps/task-cmps/task-preview.vue
+++ src/cmps/task-cmps/task-preview.vue
@@ -1,14 +1,11 @@
 <template>
   <section class="task-preview">
     <section v-if="task.labels.length" class="preview-labels">
-      💡{{task.labels.length}}
       <div
         class="preview-label"
-        v-for="label in task.labels"
-        :key="label.id"
-        :style="{backgroundColor: label.color}"
+        v-for="label in task.labels" :key="label.id"
       >
-        <span>{{label.txt}}</span>
+        <span>{{label}}</span>
       </div>
     </section>
     <h2 class="preview-title">{{task.name}}</h2>
@@ -16,26 +13,26 @@
       <div class="preview-due-date" v-if="task.dueDate">
         🕖{{task.dueDate | minimalDate}}
       </div>
-      <div class="preview-desc" v-if="task.desc">
-        📄
-      </div>
+      <div class="preview-desc" v-if="task.desc">📄</div>
       <div class="preview-comments" v-if="task.comments.length">
         💬{{task.comments.length}}
       </div>
       <div class="preview-check-list" v-if="task.checklist">
-        🗹{{checklistStatus}} 
+        🗹{{checklistStatus}}
       </div>
       <div class="preview-attachments" v-if="task.attachments.length">
         Attachments({{task.attachments.length}})
       </div>
     </section>
-     <div class="preview-members" v-if="task.members.length">
-        <div class="preview-member"
-        v-for="member in task.members" :key="member.id"> 
+    <div class="preview-members" v-if="task.members.length">
+      <div
+        class="preview-member"
+        v-for="member in task.members" 
+        :key="member.id"
+      >
           {{member.fullName}}
-        </div>
       </div>
-
+    </div>
   </section>
 </template>
 
@@ -46,16 +43,14 @@ export default {
   },
   computed: {
     checklistStatus() {
-      const todos = this.task.checklist.todos
-      const doneTodos = todos.filter(todo => todo.isDone).length
-      const allTodos = todos.length
-      return doneTodos + '/' + allTodos
-    },
-
+      const todos = this.task.checklist.todos;
+      const doneTodos = todos.filter(todo => todo.isDone).length;
+      const allTodos = todos.length;
+      return doneTodos + "/" + allTodos;
+    }
   },
   created() {
-    console.log('task' , this.task );
-    
+    console.log("task", this.task);
   }
 };
 </script>
