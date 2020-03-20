8b32bd03eb2b691fd990d7c89034cc4bd59ffc4d
Guy Amsalem <you@example.com>
2020-03-21 01:36:31 +0200

    C

---
+++ src/cmps/task-cmps/task-menu.vue
@@ -0,0 +1,17 @@
+<template>
+  <transition name="fade">
+    <section class="task-menu">
+        <div class="menu-action" v-for="(n, idx) in 4" :key="idx">
+            {{idx}}
+        </div>
+    </section>
+  </transition>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style>
+</style>
