5a1ca49e1130ae7d88be073053efdc0218bcaca8
Guy Amsalem <you@example.com>
2020-03-25 00:24:34 +0200

    task Label

--- src/cmps/task-cmps/pickers/label-picker.vue
+++ src/cmps/task-cmps/pickers/label-picker.vue
@@ -1,17 +1,24 @@
 <template>
   <section class="label-picker">
-    <header>Labels</header>
-    <div
-      class="label"
-      v-for="(label, key) in boardLabels"
-      :key="label.id"
-      :style="{'background-color': label.color}"
-      @click.stop="editLabels(key)"
-    >
-      <span class="label-txt">{{label.txt}}</span>
-      <span class="label-status" v-if="checkLabel(key)">
-        <i class="fas fa-check"></i>
-      </span>
+    <h4 class="label-header">Labels</h4>
+    <div class="label-container">
+      <div
+        class="label"
+        v-for="(label, key) in boardLabels"
+        :key="label.id"
+        @click.stop="editLabels(key)"
+      >
+        
+        <div class="label-card" :style="{'background-color': label.color}" >
+          <span class="label-txt">{{label.txt}}</span>
+          <span class="label-status" v-if="checkLabel(key)">
+            <i class="fas fa-check"></i>
+          </span>
+        </div>
+        <button class="label-edit">
+          <i class="fas fa-pencil-alt"></i>
+        </button>
+      </div>
     </div>
   </section>
 </template>
