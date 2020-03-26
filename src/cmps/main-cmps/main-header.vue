9ea3c998cb7ea343ac88240f68519538387ed06c
Rami Davidov <rami.davidov@gmail.com>
2020-03-26 12:49:18 +0200

    main-header and font

--- src/cmps/main-cmps/main-header.vue
+++ src/cmps/main-cmps/main-header.vue
@@ -1,18 +1,21 @@
 <template>
   <section class="main-header">
-    <h1 class="main-logo">taski</h1>
+    <div class="logo-container">
+      <h1 class="main-title">taski</h1>
+      <img class="main-logo" src="../../assets/logo/tasky.png" />
+    </div>
     <nav class="nav-links">
-      <router-link to="/">Home</router-link>
+      <router-link class="testu" to="/">Home</router-link>
       <router-link to="/board/5e7a6b94152ac30558217046">Board</router-link>
       <!-- <router-link to="/about">About</router-link>
       <router-link to="/dashboard">Dashboard</router-link>-->
       <div class="login-btns" v-if="isGuest">
-          <button @click="$emit('login-signup')" class="login">Login</button>
+        <button @click="$emit('login-signup')" class="login">Login</button>
         <button @click="$emit('login-signup', true)" class="signup">Signup</button>
       </div>
       <div class="user" v-if="loggedinUser">
         <router-link :to="'/user/' + loggedinUser._id">{{loggedinUser.username}}</router-link>
-      <button @click="logout" class="logout" v-if="!isGuest">Logout</button>
+        <button @click="logout" class="logout" v-if="!isGuest">Logout</button>
       </div>
     </nav>
   </section>
@@ -25,14 +28,14 @@ export default {
       return this.$store.getters.loggedinUser;
     },
     isGuest() {
-      if (!this.loggedinUser) return true
-      return this.loggedinUser.isGuest
+      if (!this.loggedinUser) return true;
+      return this.loggedinUser.isGuest;
     }
   },
   methods: {
     logout() {
-      this.$store.dispatch({type: 'logout'})
-      this.$store.dispatch({type: 'getLoggedinUser'})
+      this.$store.dispatch({ type: "logout" });
+      this.$store.dispatch({ type: "getLoggedinUser" });
     }
   }
 };
