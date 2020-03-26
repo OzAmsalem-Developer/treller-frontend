4b8812f7f9f9ae034b1a3e3cebca27468080a266
Rami Davidov <rami.davidov@gmail.com>
2020-03-26 14:31:16 +0200

    homepage

--- src/views/home-page.vue
+++ src/views/home-page.vue
@@ -1,7 +1,11 @@
 <template>
   <section class="home-page">
-    <!-- <h1>Demo1</h1> -->
-    <router-link class="cta-board-btn" to="/board/5e7a6b94152ac30558217046">Try Tasky now!</router-link>
+    <div class="hero">
+      <router-link class="cta-board-btn" to="/board/5e7a6b94152ac30558217046">Try Tasky now!</router-link>
+    </div>
+    <div>
+      <h1>Demo1</h1>
+    </div>
   </section>
 </template>
 
